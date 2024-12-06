d6c222691033004278f9b3ca5a463c847e421e07
Queenty1234 <laguerrequeenty62@gmail.com>
2024-12-05 21:51:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "2RgUiCST#JlvNeugfpFOonIvjt_LYe1wVcw7G7aIH9_d49ko3v4U",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
